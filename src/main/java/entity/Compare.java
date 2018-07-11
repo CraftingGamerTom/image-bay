9ed178705b1fa4a8dce47cca52dea2442e9b6fa7
Thomas Rokicki <tomwrhs@gmail.com>
2018-07-11 15:30:19 -0400

    Finish Compare Function

--- src/main/java/entity/Compare.java
+++ src/main/java/entity/Compare.java
@@ -4,6 +4,7 @@
 
 package entity;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,10 +34,10 @@ public class Compare {
 	 * By default the images will be compared absolutely. One different pixel
 	 * will return false
 	 * 
-	 * @param image1
-	 * @param image2
+	 * @param comparableImage
+	 * @param alphaImage
 	 */
-	public void putComparableImages(ComparableImage comparableImage, AlphaImage alphaImage) {
+	public void putComparableImage(ComparableImage comparableImage, AlphaImage alphaImage) {
 		ImageGroup imageGroup = new ImageGroup(comparableImage, alphaImage);
 		ComparableGroup comparableGroup = new ComparableGroup(imageGroup, new ComparisonOptions());
 
@@ -48,13 +49,13 @@ public class Compare {
 	 * 
 	 * Puts image into the list to be compared against the alpha image.
 	 * 
-	 * @param image1
-	 * @param image2
+	 * @param comparableImage
+	 * @param alphaImage
 	 * @param options
 	 *            The options that will specifically be used to compare the two
 	 *            images provided
 	 */
-	public void putComparableImages(ComparableImage comparableImage, AlphaImage alphaImage, ComparisonOptions options) {
+	public void putComparableImage(ComparableImage comparableImage, AlphaImage alphaImage, ComparisonOptions options) {
 		ImageGroup imageGroup = new ImageGroup(comparableImage, alphaImage);
 		ComparableGroup comparableGroup = new ComparableGroup(imageGroup, options);
 
@@ -98,6 +99,7 @@ public class Compare {
 			results = compare(comparableGroup);
 		} catch (Exception e) {
 			// TODO Handle Exceptions
+			logger.error("An error occured when comparing an image.", e);
 		}
 		return results;
 	}
@@ -113,6 +115,7 @@ public class Compare {
 				comparisonResultList.add(result);
 			} catch (Exception e) {
 				// TODO Handle Exceptions
+				logger.error("An error occured when comparing an image.", e);
 			}
 		}
 		return comparisonResultList;
@@ -128,6 +131,7 @@ public class Compare {
 	 * @return
 	 */
 	private ComparisonResult compare(ComparableGroup comparableGroup) {
+		logger.debug("Begin comparing images.");
 		List<String> comparableImageNames = new ArrayList<String>();
 		boolean isSame = true; // flag
 		List<DifferenceImage> allDifferenceImages = new ArrayList<DifferenceImage>();
@@ -146,6 +150,7 @@ public class Compare {
 
 		// Iterate through each Comparable Image to compare to the Alpha
 		for (ComparableImage imageToCompare : comparableImages) {
+			logger.debug("-- Comparing --");
 			// Put the Image name in the list for the result
 			comparableImageNames.add(imageToCompare.getName());
 
@@ -157,35 +162,156 @@ public class Compare {
 			if (imageWidth == alphaWidth && imageHeight == alphaHeight) {
 
 				// Get actual images
-				BufferedImage renderedImageToCompare = imageToCompare.getImage();
-				BufferedImage renderedAlphaImage = alphaImage.getImage();
+				BufferedImage bufferedImageToCompare = imageToCompare.getImage();
+				BufferedImage bufferedAlphaImage = alphaImage.getImage();
+				BufferedImage bufferedImageMask = comparisonOptions.getImageMask().getImage();
 
-				// TODO Add two more nested for loops to handle precision
-
-				for (int x = comparisonOptions.getStartX(); x < comparisonOptions.getEndX(); x++) {
-					for (int y = comparisonOptions.getStartY(); y < comparisonOptions.getEndY(); y++) {
-						if (renderedImageToCompare.getRGB(x, y) != renderedAlphaImage.getRGB(x, y)) {
-							// Set Flag
-							isSame = false;
-							// Create the Difference Image
-							DifferenceImage differenceImage = new DifferenceImage(
-									new BufferedImage(alphaWidth, alphaHeight, alphaImage.getType()),
-									comparisonOptions.getDiffImageName());
-							// Put the difference image into result list
-							allDifferenceImages.add(differenceImage);
-							// Save the Image
-							comparisonOptions.getDestination().writeImage(differenceImage,
-									comparisonOptions.getImageType());
-
-							// TODO Mark everything inside inner for loop as not
-							// matching
-						}
-					}
+				// Determine whether to create a Diff or a Mask
+				BufferedImage theDiffImage;
+				if (comparisonOptions.isCreateMask()) {
+					theDiffImage = new BufferedImage(alphaWidth, alphaHeight, alphaImage.getType());
+				} else {
+					theDiffImage = imageToCompare.getImage();
 				}
 
-				// TODO Handle Partial Y
+				// Ensure the precision is valid
+				int precisionWidth = comparisonOptions.getPrecision().getWidth();
+				int precisionHeight = comparisonOptions.getPrecision().getHeight();
 
-				// TODO Handle Partial X
+				if (precisionWidth < 0) {
+					precisionWidth = imageWidth;
+				}
+				if (precisionHeight < 0) {
+					precisionHeight = imageHeight;
+				}
+
+				// Determine Block ratio of area being compared
+				int widthBlocksCount = (comparisonOptions.getEndX() - comparisonOptions.getStartX()) / precisionWidth;
+				int heightBlocksCount = (comparisonOptions.getEndY() - comparisonOptions.getStartY()) / precisionHeight;
+
+				// Determine what is left over on the edge of the image
+				// int widthBlocksRemainder = (comparisonOptions.getStartY() -
+				// comparisonOptions.getStartX())
+				// % comparisonOptions.getPrecision().getWidth();
+				// int heightBlocksRemainder = (comparisonOptions.getEndY() -
+				// comparisonOptions.getEndY())
+				// % comparisonOptions.getPrecision().getHeight();
+
+				if (logger.isDebugEnabled()) {
+					logger.debug("Width block count: " + widthBlocksCount);
+					logger.debug("Height block count: " + heightBlocksCount);
+
+					logger.debug("Start X: " + comparisonOptions.getStartX());
+					logger.debug("Start Y: " + comparisonOptions.getStartY());
+					logger.debug("End X: " + comparisonOptions.getEndX());
+					logger.debug("End Y: " + comparisonOptions.getEndY());
+				}
+
+				// Define Counters
+				int pageX = comparisonOptions.getStartX();
+				int pageY = comparisonOptions.getStartY();
+
+				// Crawl the complete blocks X
+				while (pageX < comparisonOptions.getEndX()) {
+
+					// logger.debug("PageX: " + pageX);
+
+					pageY = comparisonOptions.getStartY(); // reset counter
+					// Crawl the complete blocks Y
+					while (pageY < comparisonOptions.getEndY()) {
+
+						// logger.debug("PageY: " + pageY);
+
+						/*
+						 * flag to ensure the work is not repeated when error is
+						 * found.
+						 */
+						boolean innerErrorFound = false;
+
+						// Crawl inner block X
+						for (int blockX = pageX; blockX < (pageX + precisionWidth)
+								&& blockX < comparisonOptions.getEndX(); blockX++) {
+
+							// logger.debug("BlockX: " + blockX);
+
+							// Ensure an error was not found and already handled
+							if (!innerErrorFound) {
+								// Ensure an error was not found and already
+								// handled
+								if (!innerErrorFound) {
+									// Crawl inner block Y
+									for (int blockY = pageY; blockY < (pageY + precisionHeight)
+											&& blockY < comparisonOptions.getEndY(); blockY++) {
+
+										// logger.debug("BlockY: " + blockY);
+
+										// logger.debug("Checking pixel: " +
+										// blockX + ", " + blockY);
+
+										// Ensure the area is not masked
+										if (bufferedImageMask.getRGB(blockX, blockY) == Color.TRANSLUCENT) {
+
+											// logger.debug("Not a masked
+											// pixel.");
+
+											// Handle different pixel
+											if (bufferedImageToCompare.getRGB(blockX, blockY) != bufferedAlphaImage
+													.getRGB(blockX, blockY)) {
+
+												// logger.debug("Pixel does not
+												// match.");
+
+												// Set Overall Flag
+												isSame = false;
+
+												for (int modifyBlockX = pageX; modifyBlockX < (pageX + precisionWidth)
+														&& blockX < comparisonOptions.getEndX(); modifyBlockX++) {
+													for (int modifyBlockY = pageY; modifyBlockY < (pageY
+															+ precisionHeight)
+															&& blockY < comparisonOptions.getEndY(); modifyBlockY++) {
+
+														// logger.debug("Marking
+														// errored pixel: " +
+														// modifyBlockX + ", "
+														// + modifyBlockY);
+
+														// Set the Error area
+														theDiffImage.setRGB(modifyBlockX, modifyBlockY,
+																comparisonOptions.getErrorColor());
+													}
+												}
+												// Flag to stop crawling this
+												// block
+												innerErrorFound = true;
+												break;
+											}
+										}
+									}
+								}
+							} else {
+								break; // Break loop to stop handling a mismatch
+							}
+						}
+
+						// Increment block Y-Axis
+						pageY += precisionHeight;
+					}
+					// Increment block X-Axis
+					pageX += precisionWidth;
+				}
+
+				// Create the Difference Image to be used
+				DifferenceImage differenceImage = new DifferenceImage(theDiffImage,
+						comparisonOptions.getDiffImageName() + comparisonOptions.getImageType().getExtension());
+				// Put the difference image into result list
+				allDifferenceImages.add(differenceImage);
+
+				// Only save diff if there is something different
+				if (!isSame) {
+					// Save the Image
+					comparisonOptions.getResultsDestination().writeImage(differenceImage,
+							comparisonOptions.getImageType());
+				}
 
 			} else { // Comparable image is not the same size as the alpha
 				logger.info("Image " + alphaImage.getName() + " is not the same size as the Alpha Image.");
